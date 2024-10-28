5c669760066c810eb9a097fbb10b1299d4d1fc5f
thespeace <thespeace1@gmail.com>
2024-10-28 17:23:50 +0900

    project creation

---
+++ java/1.rest-api-with-spring/src/test/java/me/thespeace/restapiwithspring/RestApiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thespeace.restapiwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
